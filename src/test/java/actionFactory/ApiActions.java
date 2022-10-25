30446e322819b80ca73a675815a71689ab60b101
suitos <suitos@naver.com>
2022-10-25 14:29:44 +0900

    add apicheck from exceldata

--- src/test/java/actionFactory/ApiActions.java
+++ src/test/java/actionFactory/ApiActions.java
@@ -1,9 +1,14 @@
 package actionFactory;
 
 import static org.testng.Assert.fail;
+import static org.hamcrest.Matchers.equalTo;
 
+import java.io.IOException;
 import java.util.Map;
 
+import org.apache.poi.EncryptedDocumentException;
+
+import apiTest.ApiTest;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
@@ -39,6 +44,21 @@ public class ApiActions {
 		protected String getValue() {
 			return value;
 		}
+		
+		public static RequestType returnRequestType() throws EncryptedDocumentException, IOException {
+			ApiTest api = new ApiTest();
+			
+			switch(api.apitestData.getCellData("RequestType")) {
+			case "POST" : return POST;
+			case "GET" : return GET;
+			case "PUT" : return PUT;
+			case "DELETE" : return DELETE;
+			
+			default : return null;
+			
+			}
+		}
+		
 	}
 
 	public ApiActions(String baseUrl) {
@@ -150,9 +170,99 @@ public class ApiActions {
 			
 		}
 
-		Logging.getLogger().info(response.asByteArray());
+		//Logging.getLogger().info(response.asByteArray());
 
 		return response;
 	}
+	
+	public Response performRequest(String serviceName, int expectedStatusCode,
+			Map<String, Object> headers, ContentType contentType, Map<String, Object> formParams,
+			Map<String, Object> queryParams, Object body, Map<String, String> cookies) throws EncryptedDocumentException, IOException {
+		String requestUrl = baseUrl + serviceName;
+
+		request = RestAssured.given().spec(requestSpec);
+		queryableRequestSpecs = SpecificationQuerier.query(request);
+
+		Logging.getLogger().info("Request URL: [" + requestUrl + "] | Request Method: [" + RequestType.returnRequestType().getValue()
+				+ "] | Expected Status Code: [" + expectedStatusCode + "]");
+
+		try {
+
+			if (headers != null) {
+				request.headers(headers);
+				String qHeaders = queryableRequestSpecs.getHeaders().toString();
+				Logging.getLogger().info("Request Headers: " + qHeaders.getBytes());
+
+			}
+
+			if (contentType != null) {
+				request.contentType(contentType);
+				String qContentType = queryableRequestSpecs.getContentType();
+				Logging.getLogger().info("Request Content Type: " + qContentType.getBytes());
+				
+			}
+
+			if (formParams != null) {
+				request.formParams(formParams);
+				String qFormParams = queryableRequestSpecs.getFormParams().toString();
+				Logging.getLogger().info("Request Form params: " + qFormParams.getBytes());
+				
+			}
+
+			if (queryParams != null) {
+				request.queryParams(queryParams);
+				String qQueryParams = queryableRequestSpecs.getQueryParams().toString();
+				Logging.getLogger().info("Request Query params: " + qQueryParams.getBytes());
+				
+			}
+
+			if (body != null) {
+				request.body(body);
+				String qBody = queryableRequestSpecs.getBody().toString();
+				Logging.getLogger().info("Request Body :" + qBody.getBytes());
+				
+			}
+
+			if (cookies != null) {
+				request.cookies(cookies);
+				String qCookies = queryableRequestSpecs.getCookies().toString();
+				Logging.getLogger().info("Request Cookies : " + qCookies.getBytes());
+				
+			}
+
+			request.filter(sessionFilter);
+			request.filter(cookieFilter);
+
+			switch (RequestType.returnRequestType()) {
+			case POST:
+				response = request.post(requestUrl);
+				break;
+			case GET:
+				response = request.get(requestUrl);
+				break;
+			case PUT:
+				response = request.put(requestUrl);
+				break;
+			case DELETE:
+				response = request.delete(requestUrl);
+				break;
+			case PATCH:
+				response = request.patch(requestUrl);
+				break;
+			}
+
+			response.then().spec(responseSpec).statusCode(expectedStatusCode);
+			
+		} catch (Exception e) {
+			Logging.getLogger().error(e.getMessage());
+			fail(e.getMessage());
+			
+		}
+
+		//Logging.getLogger().info(response.asByteArray());
+
+		return response;
+	}
+
 
 }
--- src/test/java/apiTest/ApiTest.java
+++ src/test/java/apiTest/ApiTest.java
@@ -1,9 +1,15 @@
 package apiTest;
 
 import io.restassured.response.Response;
+import utils.ExcelReader;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 import actionFactory.ApiActions;
 import actionFactory.ApiActions.RequestType;
@@ -11,6 +17,8 @@ import commonValues.Values;
 
 public class ApiTest {
 
+	public ExcelReader apitestData;
+	
 	ApiActions api = new ApiActions(Values.BASEURL);
 
 	private static final int SUCCESS = 200;
@@ -18,14 +26,37 @@ public class ApiTest {
 	// Services Names
 	private String signin_endpoint = "/login";
 
+	public ApiTest() throws EncryptedDocumentException, IOException {
+		
+		apitestData = new ExcelReader(new File("src/test/resources/testData/apiTestData.xlsx"));
+		apitestData.switchToSheet("apis");
+		
+	}
+	
 	public Response userSignInSuccess(String email,String password) {
 
 		Map<String, Object> formParams = new HashMap<>();
 		formParams.put("email", email);
 		formParams.put("password", password);
 
-		return api.performRequest(RequestType.POST, signin_endpoint, SUCCESS, null, null, formParams, null, null,
-				null);
+		return api.performRequest(RequestType.POST, signin_endpoint, SUCCESS, null, null, formParams, null, null,null);
 
+	}
+
+	public Response userSignInSuccess() throws InvalidFormatException, IOException {
+		
+		int row = apitestData.getRowCount("apis");
+		
+		String[] paramsValue = apitestData.getCellData("ParamsValue").split(",");
+		String[] params = apitestData.getCellData("FormParams").split(",");
+		
+		Map<String, Object> formParams = new HashMap<>();
+		
+		for(int i = 0; i < params.length; i++) {
+			formParams.put(params[i], paramsValue[i]);
+			
+		}
+		
+		return api.performRequest(apitestData.getCellData("ServiceName"), Integer.parseInt(apitestData.getCellData("ExpectedStatusCode")), null, null, formParams, null, null, null);
 	}
 }
